26c48ca90ac8d6cda42da7553e8bc6eec8e66664
khanak0509 <kkhanak512@gmail.com>
2025-12-25 20:49:23 +0530

    run daily

--- run.py
+++ run.py
@@ -8,12 +8,12 @@ def job():
     workflow.invoke({
         'query' : "Give me the latest top 10 news and updates about AI, new open-source repositories, emerging technologies, LangChain, Retrieval-Augmented Generation (RAG), and other major advancements in the field. For each, provide a title, description, source, and link",
         'search_results': [],
-        'email': "kkhanak513@gmail.com",
+        'email': "",
         'subject': "",
         'content': ""
     })
 
-schedule.every(5).minutes.do(job)
+schedule.every(24).hours.do(job)
 
 print("Scheduler started... Ctrl+C to stop")
 while True:
