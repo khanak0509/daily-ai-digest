c63a102375335596e77aad2826a45d2747cedfa9
khanak0509 <kkhanak512@gmail.com>
2025-12-25 20:09:45 +0530

    send mail in each 10 min

---
+++ run.py
@@ -0,0 +1,21 @@
+import schedule
+import time
+from main import workflow 
+
+
+def job():
+    print("Running AI newsletter workflow...")
+    workflow.invoke({
+        'query' : "Give me the latest top 10 news and updates about AI, new open-source repositories, emerging technologies, LangChain, Retrieval-Augmented Generation (RAG), and other major advancements in the field. For each, provide a title, description, source, and link",
+        'search_results': [],
+        'email': "kkhanak513@gmail.com",
+        'subject': "",
+        'content': ""
+    })
+
+schedule.every(5).minutes.do(job)
+
+print("Scheduler started... Ctrl+C to stop")
+while True:
+    schedule.run_pending()
+    time.sleep(60)
